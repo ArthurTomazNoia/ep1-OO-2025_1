bf2e71bd7b3b7f56c8efb004270d672a48fde87c
ArthurTomazNoia <noiaarthur29@gmail.com>
2025-05-22 21:26:29 -0300

    mexendo no modo avalicao e algumas outras coisas

--- Projeto OO remake/src/alunoEspecial.java
+++ Projeto OO remake/src/alunoEspecial.java
@@ -1,3 +1,20 @@
-public class alunoEspecial {
-    
+public class alunoEspecial extends Aluno {
+    private String nome;
+    private int matricula;
+    private String curso;
+
+    public alunoEspecial(String nome, int matricula, String curso){
+        super(nome,matricula,curso);
+
+    }
+@Override
+public void impressao(){
+    System.out.println("Nome: " + getNome());
+    System.out.println("Matricula: " + getMatricula());
+    System.out.println("Curso: " + getCurso());
+}
+@Override
+public String toString (){
+    return "Nome: " + nome + "| Matricula: " + matricula + "| Curso: " + curso;
+ }
 }
--- Projeto OO remake/src/alunoNormal.java
+++ Projeto OO remake/src/alunoNormal.java
@@ -1,3 +1,20 @@
-public class alunoNormal {
-    
+public class alunoNormal extends Aluno{
+    private String nome;
+    private int matricula;
+    private String curso;
+
+    public alunoNormal(String nome, int matricula, String curso){
+        super(nome,matricula,curso);
+
+    }
+@Override
+public void impressao(){
+    System.out.println("Nome: " + getNome());
+    System.out.println("Matricula: " + getMatricula());
+    System.out.println("Curso: " + getCurso());
+}
+@Override
+public String toString (){
+    return "Nome: " + nome + "| Matricula: " + matricula + "| Curso: " + curso;
+ }
 }
